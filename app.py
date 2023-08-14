150b1b8c0204d65a9c620e536d6235b57cf1436a
unknown <ebenezermanka1234@gmail.com>
2023-08-14 21:31:51 +0530

    app udpdated

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ def predict():
     features = [np.array(float_features)]
     prediction = model.predict(features)
 
-    return render_template("index.html",prediction_text='Final Grade Marks are likely to be :{}'.format(int(prediction)))
+    if prediction<0:
+        prediction=0
+    return render_template("index.html",prediction_text='Final Grade Marks are likely to be : {}'.format(int(prediction)))
 
 if __name__=="__main__":
     app.run(debug=True)
